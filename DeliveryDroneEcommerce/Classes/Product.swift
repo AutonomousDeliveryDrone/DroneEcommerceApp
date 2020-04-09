f3338af46c830b384e9d9d2635bca2de6a520594
Michael Peng <michael123.peng@gmail.com>
2020-04-09 11:51:56 -0700

    asf

---
+++ DeliveryDroneEcommerce/Classes/Product.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  DeliveryDroneEcommerce
+//
+//  Created by Michael Peng on 4/9/20.
+//  Copyright © 2020 Michael Peng. All rights reserved.
+//
+
+import Foundation
+
+struct Product {
+    var name : String
+    var price : Int
+    var amount : Int
+    var desc : String
+    var link: String
+    
+}
