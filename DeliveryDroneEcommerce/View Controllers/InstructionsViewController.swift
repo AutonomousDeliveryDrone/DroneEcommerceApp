d8288740ba294ab159238af48dbde3c1a5146c7a
Michael Peng <michael123.peng@gmail.com>
2020-04-08 20:31:30 -0700

    adsf

--- DeliveryDroneEcommerce/View Controllers/InstructionsViewController.swift
+++ DeliveryDroneEcommerce/View Controllers/InstructionsViewController.swift
@@ -14,8 +14,10 @@ class InstructionsViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
         //1
         self.scrollView.frame = CGRect(x:0, y:0, width:self.view.frame.width, height:self.view.frame.height)
@@ -28,14 +30,13 @@ class InstructionsViewController: UIViewController, UIScrollViewDelegate {
         self.startButton.layer.cornerRadius = 4.0
         //3
         let imgOne = UIImageView(frame: CGRect(x:0, y:0,width:scrollViewWidth, height:scrollViewHeight))
-        imgOne.image = UIImage(named: "Slide10")
+        imgOne.image = UIImage(named: "Slide 1")
         let imgTwo = UIImageView(frame: CGRect(x:scrollViewWidth, y:0,width:scrollViewWidth, height:scrollViewHeight))
         imgTwo.image = UIImage(named: "Slide 2")
         let imgThree = UIImageView(frame: CGRect(x:scrollViewWidth*2, y:0,width:scrollViewWidth, height:scrollViewHeight))
         imgThree.image = UIImage(named: "Slide 3")
-        let imgFour = UIImageView(frame: CGRect(x:scrollViewWidth*3, y:0,width:scrollViewWidth, height:scrollViewHeight))
-        imgFour.image = UIImage(named: "Slide 4")
-        
+        let imgFour = UIImageView(frame: CGRect(x:scrollViewWidth*2, y:0,width:scrollViewWidth, height:scrollViewHeight))
+        imgThree.image = UIImage(named: "Slide 4")
         self.scrollView.addSubview(imgOne)
         self.scrollView.addSubview(imgTwo)
         self.scrollView.addSubview(imgThree)
@@ -62,17 +63,18 @@ extension ScrollView
         self.pageControl.currentPage = Int(currentPage);
         // Change the text accordingly
         if Int(currentPage) == 0{
-            
-            textView.text = "Sweettutos.com is your blog of choice for Mobile tutorials"
+            textView.text = "Drone is gay"
         }else if Int(currentPage) == 1{
-            textView.text = "I write mobile tutorials mainly targeting iOS"
-        }else if Int(currentPage) == 2{
-            textView.text = "And sometimes I write games tutorials about Unity"
-        }else{
-            textView.text = "Keep visiting sweettutos.com for new coming tutorials, and don't forget to subscribe to be notified by email :)"
-            // Show the "Let's Start" button in the last slide (with a fade in animation)
+            textView.text = "Drone can fly and be free"
+            
+        }else if Int(currentPage) == 2  {
+            textView.text = "Drone can swim Drone can swim Drone can swim Drone can swim Drone can swim Drone can swim Drone can swim"
+            
+        }
+        else {
+            textView.text = "yote yote yote boat boat"
             UIView.animate(withDuration: 1.0, animations: { () -> Void in
-                self.startButton.alpha = 1.0
+                //                self.startButton.alpha = 0.5
             })
         }
     }
