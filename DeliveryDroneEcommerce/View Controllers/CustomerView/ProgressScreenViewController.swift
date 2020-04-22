04cfd1c645323c1021279e9b7f5107f82d8b622b
Michael Peng <michael123.peng@gmail.com>
2020-04-22 11:29:28 -0700

    adsf

--- DeliveryDroneEcommerce/View Controllers/CustomerView/ProgressScreenViewController.swift
+++ DeliveryDroneEcommerce/View Controllers/CustomerView/ProgressScreenViewController.swift
@@ -40,8 +40,11 @@ class ProgressScreenViewController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var comp: UILabel!
     //    @IBOutlet weak var company: UILabel!
-    var ref : DatabaseReference!
+//    var ref : DatabaseReference!
     @IBOutlet weak var name: UILabel!
+    
+    var ref : DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -100,92 +103,118 @@ class ProgressScreenViewController: UIViewController {
             self.progressBar.value = CGFloat(Double(self.distIndex)*10.0)
         }
         
-        let address = "New York, NY"
         
-        let address2 = "1063 Oaktree Drive, San Jose, CA "
-        
-        
-        var initialLat : CLLocationDegrees = 1
-        var initialLon : CLLocationDegrees = 1
-        var finalLat : CLLocationDegrees = 1
-        var finalLon : CLLocationDegrees = 1
-        
-        var geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(address) {
-            
-            placemarks, error in
-            
-            let placemark = placemarks?.first
-            let lat = placemark?.location?.coordinate.latitude
-            let lon = placemark?.location?.coordinate.longitude
-            
-            initialLat = lat!
-            initialLon = lon!
-            print("Boat: Lat: \(lat), Lon: \(lon)")
-            print(type(of: lat))
-            //            let oahuCenter = CLLocation(latitude: lat!, longitude: lon!)
-            //            let region = MKCoordinateRegion(center: oahuCenter.coordinate, latitudinalMeters: 50000, longitudinalMeters: 60000)
-            //            self.droneMap.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
-            //
-            //            let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
-            //            self.droneMap.setCameraZoomRange(zoomRange, animated: true)
-            
-            var yote = CLGeocoder()
-            yote.geocodeAddressString(address2) {
-                placemarks, error in
-                let placemark = placemarks?.first
-                let lat = placemark?.location?.coordinate.latitude
-                let lon = placemark?.location?.coordinate.longitude
-                finalLat = lat!
-                finalLon = lon!
-                print("Yote: Lat: \(lat), Lon: \(lon)")
-                
-                //                let region = MKCoordinateRegion(center: oahuCenter.coordinate, latitudinalMeters: 50000, longitudinalMeters: 60000)
-                //                self.droneMap.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
-                //
-                //                let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
-                //                self.droneMap.setCameraZoomRange(zoomRange, animated: true)
-                
-                
-                print("\(initialLat) + \(initialLon) + \(finalLat) + \(finalLon)" )
-                let diffLat : Double = Double(initialLat)-Double(finalLat)
-                let diffLon : Double = Double(initialLon)-Double(finalLon)
-                print("\(diffLat) + \(diffLon)")
-                let ind = self.distIndex
-                
-                
-                var arrLat : [Double] = []
-                var arrLon : [Double] = []
-                for i in stride(from: 1, to: 11, by: 1) {
-                    let stepLat = 0.1 * diffLat
-                    let stepLon = 0.1 * diffLon
-                    
-                    arrLat.append(Double(initialLat) - (Double(i) * stepLat))
-                    arrLon.append(Double(initialLon) - (Double(i) * stepLon))
-                    print(Double(initialLat) - (Double(i) * stepLat))
-                    print(Double(initialLon) - (Double(i) * stepLon))
+        ref.child("Orders").child("Pending").child("47").observeSingleEvent(of: .value, with: { (snapshot) in
+                guard let value = snapshot.value as? NSDictionary else {
+                    print("could not collect data")
+                    return
                 }
-                
-                
-                let oahuCenter = CLLocation(latitude: arrLat[ind], longitude: arrLon[ind])
-                let region = MKCoordinateRegion(center: oahuCenter.coordinate, latitudinalMeters: 100000, longitudinalMeters: 100000)
-                //                  self.droneMap.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
-                
-                
-                let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
-                //                self.droneMap.setCameraZoomRange(zoomRange, animated: true)
-                let annotation = MKPointAnnotation()
-                annotation.coordinate = CLLocationCoordinate2D(latitude: arrLat[ind], longitude: arrLon[ind])
-                let annotation2 = MKPointAnnotation()
-                annotation2.coordinate = CLLocationCoordinate2D(latitude: finalLat, longitude: finalLon)
-                
-                self.droneMap.addAnnotation(annotation2)
-                self.droneMap.addAnnotation(annotation)
-                
-                
-                
-            }
-        }
+            
+            let x = value["xCoord"]
+            let y = value["yCoord"]
+            
+            
+            let oahuCenter = CLLocation(latitude: x as! CLLocationDegrees, longitude: y as! CLLocationDegrees)
+            let region = MKCoordinateRegion(center: oahuCenter.coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
+                              self.droneMap.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
+            
+            
+            let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 20000)
+                            self.droneMap.setCameraZoomRange(zoomRange, animated: true)
+           
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: x as! CLLocationDegrees, longitude: y as! CLLocationDegrees)
+            
+//            self.droneMap.addAnnotation(annotation2)
+            self.droneMap.addAnnotation(annotation)
+            
+        })
+//
+//        let address = "New York, NY"
+//
+//        let address2 = "1063 Oaktree Drive, San Jose, CA "
+//
+//
+//        var initialLat : CLLocationDegrees = 1
+//        var initialLon : CLLocationDegrees = 1
+//        var finalLat : CLLocationDegrees = 1
+//        var finalLon : CLLocationDegrees = 1
+//
+//        var geocoder = CLGeocoder()
+//        geocoder.geocodeAddressString(address) {
+//
+//            placemarks, error in
+//
+//            let placemark = placemarks?.first
+//            let lat = placemark?.location?.coordinate.latitude
+//            let lon = placemark?.location?.coordinate.longitude
+//
+//            initialLat = lat!
+//            initialLon = lon!
+//            print("Boat: Lat: \(lat), Lon: \(lon)")
+//            print(type(of: lat))
+//            //            let oahuCenter = CLLocation(latitude: lat!, longitude: lon!)
+//            //            let region = MKCoordinateRegion(center: oahuCenter.coordinate, latitudinalMeters: 50000, longitudinalMeters: 60000)
+//            //            self.droneMap.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
+//            //
+//            //            let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+//            //            self.droneMap.setCameraZoomRange(zoomRange, animated: true)
+//
+//            var yote = CLGeocoder()
+//            yote.geocodeAddressString(address2) {
+//                placemarks, error in
+//                let placemark = placemarks?.first
+//                let lat = placemark?.location?.coordinate.latitude
+//                let lon = placemark?.location?.coordinate.longitude
+//                finalLat = lat!
+//                finalLon = lon!
+//                print("Yote: Lat: \(lat), Lon: \(lon)")
+//
+//                //                let region = MKCoordinateRegion(center: oahuCenter.coordinate, latitudinalMeters: 50000, longitudinalMeters: 60000)
+//                //                self.droneMap.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
+//                //
+//                //                let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+//                //                self.droneMap.setCameraZoomRange(zoomRange, animated: true)
+//
+//
+//                print("\(initialLat) + \(initialLon) + \(finalLat) + \(finalLon)" )
+//                let diffLat : Double = Double(initialLat)-Double(finalLat)
+//                let diffLon : Double = Double(initialLon)-Double(finalLon)
+//                print("\(diffLat) + \(diffLon)")
+//                let ind = self.distIndex
+//
+//
+//                var arrLat : [Double] = []
+//                var arrLon : [Double] = []
+//                for i in stride(from: 1, to: 11, by: 1) {
+//                    let stepLat = 0.1 * diffLat
+//                    let stepLon = 0.1 * diffLon
+//
+//                    arrLat.append(Double(initialLat) - (Double(i) * stepLat))
+//                    arrLon.append(Double(initialLon) - (Double(i) * stepLon))
+//                    print(Double(initialLat) - (Double(i) * stepLat))
+//                    print(Double(initialLon) - (Double(i) * stepLon))
+//                }
+//
+//
+//                let oahuCenter = CLLocation(latitude: arrLat[ind], longitude: arrLon[ind])
+//                let region = MKCoordinateRegion(center: oahuCenter.coordinate, latitudinalMeters: 100000, longitudinalMeters: 100000)
+//                //                  self.droneMap.setCameraBoundary(MKMapView.CameraBoundary(coordinateRegion: region), animated: true)
+//
+//
+//                let zoomRange = MKMapView.CameraZoomRange(maxCenterCoordinateDistance: 200000)
+//                //                self.droneMap.setCameraZoomRange(zoomRange, animated: true)
+//                let annotation = MKPointAnnotation()
+//                annotation.coordinate = CLLocationCoordinate2D(latitude: arrLat[ind], longitude: arrLon[ind])
+//                let annotation2 = MKPointAnnotation()
+//                annotation2.coordinate = CLLocationCoordinate2D(latitude: finalLat, longitude: finalLon)
+//
+//                self.droneMap.addAnnotation(annotation2)
+//                self.droneMap.addAnnotation(annotation)
+//
+//
+//
+    
         
         
         
